95017a82b8d99427261d3b50f2b6778753bd4191
Klaus-Dieter Schmatz <KlausDieter_Schmatz@iptiq.com>
2023-04-23 09:50:22 -0300

    Add filter stage

--- main.go
+++ main.go
@@ -6,17 +6,20 @@ import (
 )
 
 type job struct {
-	name string
-	data string
+	name    string
+	process bool
+	data    string
 }
 
 func main() {
 	done := make(chan any)
 	defer close(done)
-	pipeline := remove(done, upload(done, decrypt(done, download(done, list(done)))))
+	start := time.Now()
+	pipeline := remove(done, upload(done, decrypt(done, download(done, filter(done, list(done))))))
 	for v := range pipeline {
 		log.Printf("main: %s processed with result %s", v.name, v.data)
 	}
+	log.Printf("main: took %v", time.Since(start))
 }
 
 func list(done <-chan any) <-chan job {
@@ -31,43 +34,67 @@ func list(done <-chan any) <-chan job {
 			case output <- job{name: f}:
 			}
 		}
+		log.Print("list: stage complete")
 	}()
 	return output
 }
 
+func filter(done <-chan any, input <-chan job) <-chan job {
+	output := make(chan job)
+	go func() {
+		defer close(output)
+		for j := range input {
+			j.process = j.name != "file3"
+			select {
+			case <-done:
+				return
+			case output <- j:
+			}
+		}
+		log.Print("filter: stage complete")
+	}()
+	return output
+}
 func download(done <-chan any, input <-chan job) <-chan job {
 	output := make(chan job)
 	go func() {
 		defer close(output)
 		for j := range input {
-			log.Printf("ftp: GET %s", j.name)
-			time.Sleep(3 * time.Second)
+			if j.process {
+				log.Printf("ftp: GET %s", j.name)
+				time.Sleep(3 * time.Second)
+				j.data = "lengthy ciphertext"
+			}
 			select {
 			case <-done:
 				return
-			case output <- job{j.name, "lengthy ciphertext"}:
+			case output <- j:
 			}
 		}
-		log.Printf("download complete")
+		log.Print("download stage complete")
 	}()
 	return output
 }
 
-func decrypt(done <-chan any, ciphertext <-chan job) <-chan job {
-	plaintext := make(chan job)
+func decrypt(done <-chan any, input <-chan job) <-chan job {
+	output := make(chan job)
 	go func() {
-		defer close(plaintext)
-		for j := range ciphertext {
-			log.Printf("decrypt: %s", j.name)
-			time.Sleep(1 * time.Second)
+		defer close(output)
+		for j := range input {
+			if j.process {
+				log.Printf("decrypt: %s", j.name)
+				time.Sleep(1 * time.Second)
+				j.data = "lengthy plaintext"
+			}
 			select {
 			case <-done:
 				return
-			case plaintext <- job{j.name, "lenghty cleartext"}:
+			case output <- j:
 			}
 		}
+		log.Print("decrypt: stage complete")
 	}()
-	return plaintext
+	return output
 }
 
 func upload(done <-chan any, input <-chan job) <-chan job {
@@ -75,14 +102,17 @@ func upload(done <-chan any, input <-chan job) <-chan job {
 	go func() {
 		defer close(output)
 		for j := range input {
-			log.Printf("s3: PUT %s", j.name)
-			time.Sleep(3 * time.Second)
+			if j.process {
+				log.Printf("s3: PUT %s", j.name)
+				time.Sleep(3 * time.Second)
+			}
 			select {
 			case <-done:
 				return
-			case output <- job{j.name, ""}:
+			case output <- j:
 			}
 		}
+		log.Print("upload: stage complete")
 	}()
 	return output
 }
@@ -92,13 +122,15 @@ func remove(done <-chan any, input <-chan job) <-chan job {
 	go func() {
 		defer close(output)
 		for j := range input {
-			log.Printf("ftp: RM %s", j.name)
+			log.Printf("ftp: DEL %s", j.name)
+			j.data = "OK"
 			select {
 			case <-done:
 				return
-			case output <- job{j.name, "OK"}:
+			case output <- j:
 			}
 		}
+		log.Print("remove: stage complete")
 	}()
 	return output
 }
